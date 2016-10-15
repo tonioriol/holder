1bcc8f794d846bdb531ef94aaaa7d90a232340ae
Toni Oriol <tonioriol@gmail.com>
2016-10-15 04:46:08 +0200

    use facade in route closure

--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,5 @@
 <?php
 
-Route::get('holder/{height}/{width}/{color?}', 'T20n/Holder/Holder@hold');
+Route::get('holder/{height}/{width}/{color?}', function ($h, $w, $c = null) {
+	return T20n\Holder\Facades\HolderFacade::make($h, $w, $c)->response();
+});
