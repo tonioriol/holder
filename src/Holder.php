10954c43835a1b943efdd6f09af2744ca9af3086
Toni Oriol <tonioriol@gmail.com>
2016-10-15 04:43:10 +0200

    refactor

--- src/Holder.php
+++ src/Holder.php
@@ -1,42 +1,57 @@
 <?php namespace T20n\Holder;
 
+use Intervention\Image\AbstractFont;
 use Intervention\Image\ImageManager;
 
 class Holder {
-	public function hold($height, $width, $color = '#ccc') {
 
-		$min = $height > $width ? $width : $height;
+	public function make($height, $width, $color = 'ccc') {
 
-		$mul  = 3.5;
+		// validate
+		$color  = ctype_xdigit($color) ? $color : 'ccc';
+		$height = $height < 1 ? 1 : $height;
+		$width  = $width < 1 ? 1 : $width;
+
+		// set up proportions and adjustments
+		$min  = $height > $width ? $width : $height;
+		$rate = 3.5;
 		$size = $min * .2;
 
+		// params
+		$text  = "{$height} x {$width}";
+		$textX = ($width / 2) - ($size / $rate);
+		$textY = ($height / 2) - ($size / $rate);
 
-		return (new ImageManager)->canvas($width, $height, $color)
-		                         ->text("{$height} x {$width}", ($width / 2) - ($size / $mul), ($height / 2) - ($size / $mul),
-			                         function ($font) use ($size, $color) {
-				                         $font->file(__DIR__ . '../resources/alegreya-regular.ttf');
-				                         $font->size($size);
-				                         $font->color($this->invertColor($color));
-				                         $font->align('center');
-				                         $font->valign('top');
-				                         $font->angle(45);
-			                         })
-		                         ->response('png');
+		// generate
+		$image = (new ImageManager)->canvas($width, $height, $color);
+		$image->text($text, $textX, $textY, function (AbstractFont $font) use ($size, $color) {
+			$font->file(__DIR__ . '/../resources/alegreya-regular.ttf');
+			$font->size($size);
+			$font->color($this->invertColor($color));
+			$font->align('center');
+			$font->valign('top');
+			$font->angle(45);
+		});
+
+		// response
+		return $image;
 	}
 
 	private function invertColor($color) {
+
+		$color = strlen($color) === 3 ? $color . $color : $color;
+
 		$color = str_replace('#', '', $color);
 		if (strlen($color) != 6) {
-			return '000000';
+			return 'ccc';
 		}
 		$rgb = '';
-		for ($x = 0; $x < 3; $x++) {
+		for ($x = 0; $x < 3; ++$x) {
 			$c = 255 - hexdec(substr($color, (2 * $x), 2));
 			$c = ($c < 0) ? 0 : dechex($c);
 			$rgb .= (strlen($c) < 2) ? '0' . $c : $c;
 		}
 
 		return '#' . $rgb;
-
 	}
 }
